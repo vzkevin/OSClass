b38f1cffe76c8466ef476566c834110855265e20
Kevin Horta <kevinhorta3@gmail.com>
2024-05-23 19:58:36 -0400

    Update runpipe.cpp

--- runpipe.cpp
+++ runpipe.cpp
@@ -1,82 +1,103 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <sstream>
+#include <vector>
+#include <string>
+#include <cstring>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <cstdlib>
 
 using namespace std;
 
-// Function to split a string by delimiter
-vector<string> split(const string &s, char delimiter) {
-    vector<string> tokens;
-    string token;
-    istringstream tokenStream(s);
-    while (getline(tokenStream, token, delimiter)) {
-        tokens.push_back(token);
-    }
-    return tokens;
-}
-
-int main() {
-    string input;
-
-    // Print prompt and read input
-    cout << "Enter a pipeline command: ";
-    getline(cin, input);
-
-    // Parse the input into individual commands
-    vector<string> commands = split(input, '|');
+void execute_command(vector<string>& commands) {
     int num_commands = commands.size();
+    int pipe_fds[num_commands - 1][2];  // Pipes array
 
-    // Array of file descriptors for the pipe
-    int pipe_fd[2];
-    int prev_fd = -1; // File descriptor for the previous command
+    // Create pipes
+    for (int i = 0; i < num_commands - 1; ++i) {
+        if (pipe(pipe_fds[i]) == -1) {
+            cerr << "Pipe creation failed\n";
+            exit(1);
+        }
+    }
 
     for (int i = 0; i < num_commands; ++i) {
-        pipe(pipe_fd); // Create a pipe
-
         pid_t pid = fork();
+
         if (pid == -1) {
-            cerr << "Failed to fork" << endl;
-            return 1;
-        }
-
-        if (pid == 0) {
+            cerr << "Fork failed\n";
+            exit(1);
+        } else if (pid == 0) {
             // Child process
-            if (prev_fd != -1) {
-                dup2(prev_fd, 0); // Redirect stdin to the previous pipe's read end
-                close(prev_fd);
-            }
-            if (i < num_commands - 1) {
-                dup2(pipe_fd[1], 1); // Redirect stdout to the current pipe's write end
-                close(pipe_fd[1]);
-            }
-            close(pipe_fd[0]);
 
-            // Parse the command and its arguments
-            vector<string> cmd_args = split(commands[i], ' ');
+            // If not the first command, set the input from the previous pipe
+            if (i > 0) {
+                if (dup2(pipe_fds[i - 1][0], STDIN_FILENO) == -1) {
+                    cerr << "dup2 input failed\n";
+                    exit(1);
+                }
+            }
+
+            // If not the last command, set the output to the next pipe
+            if (i < num_commands - 1) {
+                if (dup2(pipe_fds[i][1], STDOUT_FILENO) == -1) {
+                    cerr << "dup2 output failed\n";
+                    exit(1);
+                }
+            }
+
+            // Close all pipe file descriptors in child processes
+            for (int j = 0; j < num_commands - 1; ++j) {
+                close(pipe_fds[j][0]);
+                close(pipe_fds[j][1]);
+            }
+
+            // Parse command and its arguments
             vector<char*> args;
-            for (const string& arg : cmd_args) {
-                args.push_back((char*)arg.c_str());
+            stringstream ss(commands[i]);
+            string arg;
+            while (ss >> arg) {
+                args.push_back(strdup(arg.c_str()));
             }
             args.push_back(nullptr);
 
-            // Execute the command
+            // Execute command
             execvp(args[0], args.data());
-            cerr << "Failed to execute " << args[0] << endl;
+            cerr << "Command execution failed: " << args[0] << '\n';
             exit(1);
-        } else {
-            // Parent process
-            waitpid(pid, nullptr, 0); // Wait for the child process to finish
-            close(pipe_fd[1]); // Close the write end of the pipe
-            if (prev_fd != -1) {
-                close(prev_fd);
-            }
-            prev_fd = pipe_fd[0]; // Save the read end of the pipe for the next command
         }
     }
+
+    // Close all pipe file descriptors in the parent process
+    for (int i = 0; i < num_commands - 1; ++i) {
+        close(pipe_fds[i][0]);
+        close(pipe_fds[i][1]);
+    }
+
+    // Wait for all child processes to finish
+    int status;
+    for (int i = 0; i < num_commands; ++i) {
+        wait(&status);
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+            exit(WEXITSTATUS(status));
+        }
+    }
+}
+
+int main() {
+    cout << "Enter a pipeline command: ";
+    string command_line;
+    getline(cin, command_line);
+
+    stringstream ss(command_line);
+    string segment;
+    vector<string> commands;
+
+    // Split the command line by '|'
+    while (getline(ss, segment, '|')) {
+        commands.push_back(segment);
+    }
+
+    execute_command(commands);
 
     return 0;
 }
