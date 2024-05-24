3fabe0369cddb3ed1e6050fa62d93f4aa66775f0
Kevin Horta <kevinhorta3@gmail.com>
2024-05-24 19:09:10 -0400

    test definitly revert

--- runpipe.cpp
+++ runpipe.cpp
@@ -1,36 +1,40 @@
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <string>
-#include <cstring>
-#include <unistd.h>
-#include <sys/wait.h>
-
-using namespace std;
-
 void execute_command(vector<vector<string>>& commands) {
     int num_commands = commands.size();
     int pipe_fds[num_commands - 1][2];  // Pipes array
 
     // Create pipes
     for (int i = 0; i < num_commands - 1; ++i) {
-        pipe(pipe_fds[i]);
+        if (pipe(pipe_fds[i]) == -1) {
+            perror("pipe");
+            exit(1);
+        }
     }
 
     for (int i = 0; i < num_commands; ++i) {
         pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("fork");
+            exit(1);
+        }
 
         if (pid == 0) {
             // Child process
 
             // If not the first command, set the input from the previous pipe
             if (i > 0) {
-                dup2(pipe_fds[i - 1][0], STDIN_FILENO);
+                if (dup2(pipe_fds[i - 1][0], STDIN_FILENO) == -1) {
+                    perror("dup2");
+                    exit(1);
+                }
             }
 
             // If not the last command, set the output to the next pipe
             if (i < num_commands - 1) {
-                dup2(pipe_fds[i][1], STDOUT_FILENO);
+                if (dup2(pipe_fds[i][1], STDOUT_FILENO) == -1) {
+                    perror("dup2");
+                    exit(1);
+                }
             }
 
             // Close all pipe file descriptors in child processes
@@ -47,6 +51,7 @@ void execute_command(vector<vector<string>>& commands) {
             args.push_back(nullptr);
 
             execvp(args[0], args.data());
+            perror("execvp");
             exit(1);
         }
     }
@@ -62,41 +67,4 @@ void execute_command(vector<vector<string>>& commands) {
     for (int i = 0; i < num_commands; ++i) {
         waitpid(-1, &status, 0);
     }
-}
-
-int main() {
-    while (true) {
-        cout << "Enter a pipeline command (or 'exit' to quit): ";
-        string command_line;
-        getline(cin, command_line);
-
-        if (command_line == "exit") {
-            break;
-        }
-
-        vector<vector<string>> commands;
-        stringstream ss(command_line);
-        string segment;
-        vector<string> command_args;
-
-        // Split the command line into segments
-        while (getline(ss, segment, '|')) {
-            stringstream command_stream(segment);
-            string arg;
-
-            // Split each segment into command and arguments
-            while (command_stream >> arg) {
-                command_args.push_back(arg);
-            }
-
-            // Add this command and its arguments to the commands vector
-            commands.push_back(command_args);
-            // Clear the command_args vector for the next command
-            command_args.clear();
-        }
-
-        execute_command(commands);
-    }
-
-    return 0;
 }
