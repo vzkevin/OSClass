938ab50b168ac19027075482241ded2316d74102
Kevin Horta <kevinhorta3@gmail.com>
2024-05-24 18:25:22 -0400

    correct for input #include

--- runpipe.cpp
+++ runpipe.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-void execute_command(vector<string>& commands) {
+void execute_command(vector<vector<string>>& commands) {
     int num_commands = commands.size();
     int pipe_fds[num_commands - 1][2];  // Pipes array
 
@@ -20,26 +20,17 @@ void execute_command(vector<string>& commands) {
     for (int i = 0; i < num_commands; ++i) {
         pid_t pid = fork();
 
-        if (pid == -1) {
-            cerr << "Fork failed\n";
-            exit(1);
-        } else if (pid == 0) {
+        if (pid == 0) {
             // Child process
 
             // If not the first command, set the input from the previous pipe
             if (i > 0) {
-                if (dup2(pipe_fds[i - 1][0], STDIN_FILENO) == -1) {
-                    cerr << "dup2 input failed\n";
-                    exit(1);
-                }
+                dup2(pipe_fds[i - 1][0], STDIN_FILENO);
             }
 
             // If not the last command, set the output to the next pipe
             if (i < num_commands - 1) {
-                if (dup2(pipe_fds[i][1], STDOUT_FILENO) == -1) {
-                    cerr << "dup2 output failed\n";
-                    exit(1);
-                }
+                dup2(pipe_fds[i][1], STDOUT_FILENO);
             }
 
             // Close all pipe file descriptors in child processes
@@ -48,27 +39,28 @@ void execute_command(vector<string>& commands) {
                 close(pipe_fds[j][1]);
             }
 
+            // Convert vector<string> to vector<char*> for execvp
             vector<char*> args;
-            stringstream ss(commands[i]);
-            string arg;
-            while (ss >> arg) {
-                args.push_back(strdup(arg.c_str()));
+            for (auto& arg : commands[i]) {
+                args.push_back(const_cast<char*>(arg.c_str()));
             }
             args.push_back(nullptr);
 
             execvp(args[0], args.data());
-            cerr << "Command execution failed: " << args[0] << '\n';
             exit(1);
         }
     }
 
+    // Close all pipe file descriptors in the parent process
     for (int i = 0; i < num_commands - 1; ++i) {
         close(pipe_fds[i][0]);
         close(pipe_fds[i][1]);
     }
 
+    // Wait for all child processes to finish
+    int status;
     for (int i = 0; i < num_commands; ++i) {
-        wait(nullptr);
+        waitpid(-1, &status, 0);
     }
 }
 
@@ -79,11 +71,20 @@ int main() {
 
     stringstream ss(command_line);
     string segment;
-    vector<string> commands;
+    vector<vector<string>> commands;
 
     // Split the command line by '|'
     while (getline(ss, segment, '|')) {
-        commands.push_back(segment);
+        stringstream command_stream(segment);
+        string arg;
+        vector<string> command_args;
+
+        // Split the segment into command and arguments
+        while (command_stream >> arg) {
+            command_args.push_back(arg);
+        }
+
+        commands.push_back(command_args);
     }
 
     execute_command(commands);
